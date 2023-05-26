3fb76c108eeb9356025279b4c62258bd045ead69
hakim-asrori <saneglos005@gmail.com>
2023-05-26 19:00:26 +0700

    Upload Project

---
+++ app/Console/Commands/CrawlingYoutube.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CrawlingYoutube extends Command
+{
+    protected $signature = 'app:crawling-youtube';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        //
+    }
+}
